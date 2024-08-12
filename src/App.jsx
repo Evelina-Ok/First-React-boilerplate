e704927b533cc70b1189a344d31f99656d474a25
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-08-12 13:58:03 +0200

    #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import './App.css'
+import './App.scss'
 import { Greetings } from "./components/Greetings"
 import { Main } from "./components/Main"
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
-import { Navbar } from "./components/Navbar"
+import { Navbar } from "./components/Navbar/Navbar"
  
 function App() {
   
   return (
   <>
  
-  <Greetings/>
-  <Main/>
+  {/* <Greetings/> */}
+
   <Header/>
   <Navbar/>
+  <Main/>
   <Footer/>
   </>
   )
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import style from "./Main.module.scss"
+
+
+export const Main = () => {
+    return (
+    <>
+    <main className={style.mainStyle}>
+        <h1>Today is going to be a great day!</h1>
+        <h2>And this is a Main component </h2>
+    </main>
+    </>
+    )
+}
