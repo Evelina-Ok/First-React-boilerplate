58a71546558b0099eb71a6d770b62d019825df58
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-08-13 10:00:29 +0200

    #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.scss'
 import { Greetings } from "./components/Greetings"
-import { Main } from "./components/Main"
-import { Header } from "./components/Header"
-import { Footer } from "./components/Footer"
+import { Main } from "./components/Main/Main"
+import { Header } from "./components/Header/Header"
+import { Footer } from "./components/Footer/Footer"
 import { Navbar } from "./components/Navbar/Navbar"
  
 function App() {
